edd4fe0615947dd0ec000f0df480c29373023b30
Azibatayam <95300506+Azibatayam@users.noreply.github.com>
2022-04-26 19:54:48 +0200

    Greater integer

---
+++ index.js
@@ -0,0 +1,12 @@
+/* EXERCISE 1
+ Write a piece of code to find the largest of  given two integers
+*/
+
+let firstInteger = 500
+let secondInteger = 700
+
+if (firstInteger > secondInteger){
+ console.log("Greater");
+}else{
+    console.log("Smaller");
+}

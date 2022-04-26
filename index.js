e34e78fa0c16c016efe5ebc81b5c1354cad44877
Azibatayam <95300506+Azibatayam@users.noreply.github.com>
2022-04-26 20:04:38 +0200

    Equality

--- index.js
+++ index.js
@@ -2,11 +2,21 @@
  Write a piece of code to find the largest of  given two integers
 */
 
-let firstInteger = 500
-let secondInteger = 700
+let firstInteger = 500;
+let secondInteger = 700;
 
 if (firstInteger > secondInteger){
  console.log("Greater");
 }else{
     console.log("Smaller");
+}
+
+/* EXERCISE 2
+  Write a piece of code to check: if  given an integer is NOT equal to 5 then display " not equal"
+*/
+
+let table = 5;
+let tableTwo = "5"
+    if (!(table === tableTwo)){
+console.log("Not equal" );
 }
